dd2161484e1ab40cfbd2df9c0296083e02fff50c
Bell <bell_andrew@icloud.com>
2020-07-13 08:57:08 -0700

    Receiver Code Done

---
+++ src/main/java/cst438/assig3reservation/Assig3ReservationApplication.java
@@ -0,0 +1,13 @@
+package cst438.assig3reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assig3ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assig3ReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cst438/assig3reservation/Assig3ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438.assig3reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assig3ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
